380b72916fddc080e5e3c206a1ee01505b1d791f
Sanyok6 <tishchenkoa2008@gmail.com>
2022-10-08 15:46:16 -0400

    Load last puzzle user worked on when set is opened

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export interface Task {
 	id: number;
@@ -40,3 +41,10 @@ export interface Puzzles {
 export const userStore = writable<User | null>(null);
 
 export const taskClaimed = writable<Boolean>(true);
+
+export const puzzleIndex = writable<number>(0);
+
+if (browser) {
+	puzzleIndex.set(localStorage.puzzleIndex || 0)
+	puzzleIndex.subscribe((value) => localStorage.puzzleIndex = value)
+}
--- frontend/src/routes/dashboard/puzzles/[set]/+page.svelte
+++ frontend/src/routes/dashboard/puzzles/[set]/+page.svelte
@@ -2,21 +2,13 @@
     import { onMount } from 'svelte';
     import { fetchApi, fetchUserData } from '$lib/api';
 	import { page } from '$app/stores';
-    import { userStore, type PuzzleSet, type User } from '$lib/store';
+    import { userStore, type PuzzleSet, type User, puzzleIndex } from '$lib/store';
 	import { Chessground, cgStylesHelper, type Api } from 'svelte-use-chessground';
 	import 'svelte-use-chessground/cgstyles/chessground.css';
 
 	let userData: User | null = null;
 	let currentSet: PuzzleSet | null = null; 
 
-    userStore.subscribe(d => {
-		userData = d;
-		if (userData) {
-			for (let p in userData.data.sets) {
-				if (userData.data.sets[p].id == $page.params.set) {currentSet = userData.data.sets[p]}
-			}
-		}
-	})
 
 	let createModalOpen = false
 
@@ -49,9 +41,10 @@
 		cgApi.state.movable.dests = validMovesAsDests();
 	}
 
-	let feedback = {state: "starting", response: "", animate: false}
+	let feedback = {state: "starting", response: "", animate: false, solution: "", current_puzzle: 0}
 
     import { Chess } from 'chess.ts'
+    import { to_number } from 'svelte/internal';
 
 
     const chess = new Chess()
@@ -145,6 +138,7 @@
 				newFEN = ""
 				newPGN = ""
 				fetchUserData()
+				startPuzzle(currentSet?.puzzles.length-1)
 			} else {
 				alert("Error sending data to server.") 
 			}
@@ -157,12 +151,24 @@
 				newPGN = formattedPGN.split("\n").reverse()[0].replace(" *", "")
 				moves = loadPGN(newPGN)
 				create()
-			} else messages = ["Error loading moves, make sure your correct variations are in the right format."]
+			} else messages = ["Error loading correct variations, make sure the moves are in the right format."]
 
 		} else messages = ["Invalid Starting Position"]
 
     }
-	
+
+	const deletePuzzle = async (i: number) => {
+		const response = await fetchApi("puzzlesets/puzzles/"+currentSet?.puzzles[i].id, {
+            method: "DELETE",
+        });
+        if (response.ok) {
+			cgApi.set({fen: ""})
+			feedback.current_puzzle = 0
+            fetchUserData()
+        } else {
+            alert("Error deleting puzzle.")
+        }
+	}
 
 	const startPuzzle = (index: number) => {
 		const puzzle = currentSet?.puzzles[index]
@@ -177,6 +183,9 @@
 		config.movable.events.after = afterMove
 
 		feedback.state = "start"
+		feedback.current_puzzle = index
+
+		puzzleIndex.set(index)
 	}
 
 	const afterMove = (from: string, to: string, metadata: string) => {
@@ -185,10 +194,12 @@
 
 		let correctMoves: string[] = [] // {move:"", response:""}
 		let responses: string[] = []
+		let after: string[] = []
 		goThroughEach((m, c, [a, b]) => {
 			if (currentPosition == c.fen().split(" ")[0]) {
 				correctMoves.push(m)
 				responses.push(moves[a].moves[b+1] || "")	
+				after.push(moves[a].moves[b+2] || "")
 			}
 			return true
 		})
@@ -203,6 +214,11 @@
 					cgApi.move(response?.from, response?.to);
 					cgApi.state.movable.dests = validMovesAsDests();
 					cgApi.playPremove();
+					feedback.solution = after[moveIndex]
+					if (!after[moveIndex]) {
+						feedback.state = "done"
+						feedback.solution = ""
+					}
 				}, 300);
 			} else {
 				feedback.state = "done"
@@ -222,6 +238,16 @@
 		setTimeout(() => feedback.animate = false, 500)
 
 	}
+
+	userStore.subscribe(d => {
+		userData = d;
+		if (userData) {
+			for (let p in userData.data.sets) {
+				if (userData.data.sets[p].id == $page.params.set) {currentSet = userData.data.sets[p]}
+			}
+			startPuzzle(to_number(localStorage.getItem("puzzleIndex"))	|| 0)
+		}
+	})
 
 </script>
 
@@ -247,21 +273,22 @@
 				</button>
 			</div>
 		</div>
-		<div class="col-span-2 overflow-y-scroll">
+		<div class="h-full overflow-y-scroll">
 			{#each currentSet.puzzles as s, i}
-				<div on:click={() => {startPuzzle(i)}} class="border-2 rounded-lg m-3 p-2 flex justify-between border-gray-400 dark:border-gray-300 hover:bg-gray-300 dark:hover:bg-gray-800">
+				<div on:click={() => {startPuzzle(i)}} class="border-2 rounded-lg m-3 p-2 flex justify-between border-gray-400 dark:border-gray-300 hover:bg-blue-500/40 {feedback.current_puzzle==i ? 'bg-blue-500/25' : ''}">
 					<div class="flex flex-nowrap">
-						<div class="mt-0.5 text-green-400">
+						<!-- <div class="mt-0.5 text-green-600 dark:text-green-400">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
 								<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
 							</svg>
-						</div>
+						</div> -->
 						<p class="text-lg mx-1">Puzzle {i+1}</p>
 					</div>
-					<div class="mt-1 hover:text-blue-600 dark:hover:text-blue-200">
-						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-							<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-						</svg>
+					<div on:click={() => {deletePuzzle(i)}} class="mt-1 hover:text-red-600 dark:hover:text-red-400">
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+							<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+						  </svg>
 					</div>
 				</div>
 			{/each}
