ac0778ddcca2b6705c71947c2a8c7c2a8678d54e
Sanyok6 <tishchenkoa2008@gmail.com>
2022-10-21 19:28:01 -0400

    remove adaptor

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,4 @@
 import vercel from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +8,6 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
 		adapter: vercel(),
 	}
 };
