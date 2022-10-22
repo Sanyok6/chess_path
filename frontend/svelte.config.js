9162923648844f3ecf9dae72a029db9ec2333225
Sanyok6 <tishchenkoa2008@gmail.com>
2022-10-21 21:57:06 -0400

    actually work plz

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,6 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: vercel(),
+		adapter: adapter()
 	}
 };
 
